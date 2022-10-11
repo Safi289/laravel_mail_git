7827c12becd6495cab21d5f7bbcc09200cdb4937
Safi289 <ssahid151289@bscse.uiu.ac.bd.com>
2022-10-11 17:14:35 +0600

    first commit

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SignUp;
+
+class MailController extends Controller
+{
+    public function sendMail()
+    {
+    	$name = 'Safi';
+    	Mail::to('fake@mail.com')->send(new SignUp($name));
+    	return view('welcome');
+    }
+}
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg" class="logo" alt="Laravel Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
